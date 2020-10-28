7fbd1e56657ee4fc9f22b0c15f2c96c5ec941ef9
nasriddinovumidjon <vebdeveloper571632@mail.ru>
2020-10-28 18:09:20 +0500

    Some changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -21,6 +21,4 @@ function App() {
     </div>
   );
 }
-
-
 export default App;
